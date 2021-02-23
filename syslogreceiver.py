bb1b390095b8343d6444654ce95c3ec9afe9b51f
dfirale <aleksii.vuorinen@hotmail.com>
2021-02-23 21:18:58 +0200

    Update syslogreceiver.py

--- syslogreceiver.py
+++ syslogreceiver.py
@@ -5,10 +5,9 @@
 ## This is a tiny syslog server that is able to receive UDP based syslog
 ## entries on a specified port and save them to a file.
 ## That's it... it does nothing else...
-## If you are in python3, simply change SocketServer to socketserver
 ## There are a few configuration parameters.
 
-LOG_FILE = 'youlogfile.log'
+LOG_FILE = 'events.log'
 HOST, PORT = "0.0.0.0", 514
 
 #
@@ -16,11 +15,11 @@ HOST, PORT = "0.0.0.0", 514
 #
 
 import logging
-import SocketServer
+import socketserver
 
 logging.basicConfig(level=logging.INFO, format='%(message)s', datefmt='', filename=LOG_FILE, filemode='a')
 
-class SyslogUDPHandler(SocketServer.BaseRequestHandler):
+class SyslogUDPHandler(socketserver.BaseRequestHandler):
 
 	def handle(self):
 		data = bytes.decode(self.request[0].strip())
@@ -30,7 +29,7 @@ class SyslogUDPHandler(SocketServer.BaseRequestHandler):
 
 if __name__ == "__main__":
 	try:
-		server = SocketServer.UDPServer((HOST,PORT), SyslogUDPHandler)
+		server = socketserver.UDPServer((HOST,PORT), SyslogUDPHandler)
 		server.serve_forever(poll_interval=0.5)
 	except (IOError, SystemExit):
 		raise
