c551504002d0fc0f4b962bc900c53ecff4a70ef0
dfirale <aleksii.vuorinen@hotmail.com>
2021-02-23 20:39:23 +0200

    Create sysloglistener.ps1

---
+++ sysloglistener.ps1
@@ -0,0 +1,22 @@
+# Writes data received from $port to $file
+# Default syslog port = 514
+# usage:
+# ./sysloglistener.ps1 -port 514 -file C:\Users\user01\Desktop\syslog.txt
+
+param (
+    $port,
+    $file
+)
+
+$endpoint = New-Object System.Net.IPEndPoint ([IPAddress]::Any, $port)
+Try {
+    while($true) {
+        $socket = New-Object System.Net.Sockets.UdpClient $port
+        $content = $socket.Receive([ref]$endpoint)
+        $socket.Close()
+        [Text.Encoding]::ASCII.GetString($content) | Add-Content -Path $file
+    }
+
+} Catch {
+    "$($Error[0])"
+}
